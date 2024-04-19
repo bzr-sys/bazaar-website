eb6257e9774964ad08f299dcf57c7ece1c51bae9
Paddy <ohanlon.paddy@gmail.com>
2024-04-19 11:57:42 +0200

    Remove env var service name

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,8 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
-const serviceName = import.meta.env.PUBLIC_SERVICE_NAME;
 
 export async function GET(context) {
   return rss({
-    title: `${serviceName} | Blog`,
+    title: `Bazaar | Blog`,
     description: "Tutorials and news.",
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
