011838aad394a48a8dcb4b1f7165536b492c77d1
Sascha Trifunovic <sascha.trifunovic@gmail.com>
2024-11-07 10:29:55 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,36 +4,43 @@ import starlight from "@astrojs/starlight";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), starlight({
-    title: "Bazaar Docs",
-    customCss: ["./src/styles/starlight.css"],
-    components: {
-      SiteTitle: "./src/components/starlight/SiteTitle.astro",
-      ThemeSelect: "./src/components/starlight/ThemeSelector.astro",
-    },
-    sidebar: [
-      {
-        label: "Start Here",
-        autogenerate: {
-          directory: "docs/start-here",
-        },
-      },
-      {
-        label: "Guides",
-        autogenerate: {
-          directory: "docs/guides",
-        },
-      },
-      {
-        label: "Features",
-        autogenerate: {
-          directory: "docs/features",
-        },
-      },
-      { label: "API Reference", link: "/api" },
-    ],
-  }), mdx()],
   site: "https://bzr.dev",
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    starlight({
+      title: "Bazaar Docs",
+      customCss: ["./src/styles/starlight.css"],
+      components: {
+        SiteTitle: "./src/components/starlight/SiteTitle.astro",
+        ThemeSelect: "./src/components/starlight/ThemeSelector.astro",
+      },
+      sidebar: [
+        {
+          label: "Start Here",
+          autogenerate: {
+            directory: "docs/start-here",
+          },
+        },
+        {
+          label: "Guides",
+          autogenerate: {
+            directory: "docs/guides",
+          },
+        },
+        {
+          label: "Features",
+          autogenerate: {
+            directory: "docs/features",
+          },
+        },
+        { label: "API Reference", link: "/api" },
+      ],
+    }),
+  ],
 });
